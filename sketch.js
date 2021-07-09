ae3476cb429d0f08ce93ac05cdafe0510f9775d1
Phantom09042007 <68319148+Phantom09042007@users.noreply.github.com>
2021-07-09 15:48:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var canvas;
+var backgroundIMG, gameState = 0, playerCount, database, form, player, game;
+
+function setup(){
+    canvas = createCnavas(400,400);
+    database = firebase.database();
+}
+
+function draw(){
+
+}
